9fbdf1a060cff8fa2faff98b568155d9301fa34a
sho130 <sho@aka-megane.com>
2019-03-01 21:07:57 +0900

    add DeleteUser

--- user.go
+++ user.go
@@ -74,3 +74,30 @@ func (c *Client) User(id int) (*User, error) {
 	}
 	return &r.User, nil
 }
+
+func (c *Client) DeleteUser(id int) error {
+	req, err := http.NewRequest("DELETE", c.endpoint+"/users/"+strconv.Itoa(id)+".json?key="+c.apikey, strings.NewReader(""))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	res, err := c.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode == 404 {
+		return errors.New("Not Found")
+	}
+
+	decoder := json.NewDecoder(res.Body)
+	if res.StatusCode != 200 {
+		var er errorsResult
+		err = decoder.Decode(&er)
+		if err == nil {
+			err = errors.New(strings.Join(er.Errors, "\n"))
+		}
+	}
+	return err
+}
