4c7506d2f20d4a6e160cb75c3d21744ccb1ed383
sho130 <sho@aka-megane.com>
2020-03-15 18:31:48 +0900

    add group

---
+++ group.go
@@ -0,0 +1,36 @@
+package redmine
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+func (c *Client) DeleteGroup(id int) error {
+	req, err := http.NewRequest("DELETE", c.endpoint+"/groups/"+strconv.Itoa(id)+".json?key="+c.apikey, strings.NewReader(""))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	res, err := c.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode == 404 {
+		return errors.New("Not Found")
+	}
+
+	decoder := json.NewDecoder(res.Body)
+	if res.StatusCode != 200 {
+		var er errorsResult
+		err = decoder.Decode(&er)
+		if err == nil {
+			err = errors.New(strings.Join(er.Errors, "\n"))
+		}
+	}
+	return err
+}
